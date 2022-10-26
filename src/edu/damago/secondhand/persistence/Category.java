eb97fcb8cf8d7aa692f8e791250c75c6962bc20b
AyuCalices <r.jaspers18@gmail.com>
2022-10-26 15:13:19 +0200

    implemented base classes with getter and setter

---
+++ src/edu/damago/secondhand/persistence/Category.java
@@ -0,0 +1,24 @@
+package edu.damago.secondhand.persistence;
+
+public enum Category {
+    ART,
+    CRAFT,
+    HANDICRAFT,
+    GOURMET,
+    BEAUTY,
+    HEALTH,
+    INFANT,
+    PET,
+    COMPUTER,
+    PHONE,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+    COLLECTIBLE,
+    VEHICLE,
+    TOY,
+    INDUSTRIAL,
+    STATIONERY,
+    DOMESTIC,
+    GARDEN
+}
