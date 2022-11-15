ff958f934f253bbbf83a08b9540047c3f69709b3
Christopher Knapp <chris.knapp@live.de>
2022-11-16 00:12:16 +0100

    add service method to get a person by id

--- src/edu/htw/secondhand/service/PersonService.java
+++ src/edu/htw/secondhand/service/PersonService.java
@@ -1,14 +1,31 @@
 package edu.htw.secondhand.service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import edu.htw.secondhand.persistence.Person;
+import edu.htw.secondhand.util.RestJpaLifecycleProvider;
 
-@Path("person")
+import javax.persistence.EntityManager;
+import javax.validation.constraints.PositiveOrZero;
+import javax.ws.rs.*;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+@Path("people")
 public class PersonService {
 
     @GET
-    public String getPerson() {
-        return "person";
+    @Path("{id}")
+    @Produces({ TEXT_PLAIN })
+    public Person getPersonById(
+        @PathParam("id") @PositiveOrZero long personIdentity,
+        @HeaderParam("X-Requester-Identity") long requesterIdentity
+    ) {
+        EntityManager entityManager = RestJpaLifecycleProvider.entityManager("secondhand");
+        long identity = personIdentity == 0 ? requesterIdentity : personIdentity;
+        Person person = entityManager.find(Person.class, identity);
+        if (person == null) {
+            throw new ClientErrorException(404);
+        }
+        return person;
     }
 
 }
