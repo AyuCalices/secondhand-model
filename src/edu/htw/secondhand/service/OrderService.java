38757369446f146de9bd422a644b61295ed4100f
AyuCalices <r.jaspers18@gmail.com>
2022-11-25 15:08:01 +0100

    added fist order post

--- src/edu/htw/secondhand/service/OrderService.java
+++ src/edu/htw/secondhand/service/OrderService.java
@@ -7,18 +7,64 @@ import edu.htw.secondhand.persistence.Person;
 import edu.htw.secondhand.util.RestJpaLifecycleProvider;
 
 import javax.persistence.EntityManager;
+import javax.persistence.RollbackException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.Set;
 
+import static edu.htw.secondhand.service.BasicAuthenticationReceiverFilter.REQUESTER_IDENTITY;
+import static javax.ws.rs.core.Response.Status.*;
+
 @Path("order")
 public class OrderService {
 
     @GET
-    public String postOrder() {
-        //TODO: tbd
-        return "order";
+    public long postOrder(
+            @HeaderParam(REQUESTER_IDENTITY) @Positive final long requesterIdentity,
+            @QueryParam("avatarReference") @Positive Long avatarReference,
+            @NotNull @Valid Order orderTemplate
+    ) {
+        final EntityManager entityManager = RestJpaLifecycleProvider.entityManager("secondhand");
+        final Person requester = entityManager.find(Person.class, requesterIdentity);
+        if (requester == null) throw new ClientErrorException(FORBIDDEN);
+
+        final boolean insertMode = orderTemplate.getIdentity() == 0;
+        boolean isRequesterAdmin = requester.getGroup() == Group.ADMIN;
+
+        if (isRequesterAdmin) throw new ClientErrorException(FORBIDDEN);
+
+        final Order order;
+        if (insertMode) {
+            order = new Order(requester);
+            if (avatarReference == null) avatarReference = 1L;
+        } else {
+            order = entityManager.find(Order.class, orderTemplate.getIdentity());
+            if (order == null) throw new ClientErrorException(NOT_FOUND);
+        }
+
+        order.setArrived(orderTemplate.getArrived());
+        order.setDeparted(orderTemplate.getDeparted());
+        order.setPayed(orderTemplate.getPayed());
+        order.setTrackingReference(orderTemplate.getTrackingReference());
+
+        if (insertMode)
+            entityManager.persist(order);
+        else
+            entityManager.flush();
+
+        try {
+            entityManager.getTransaction().commit();
+        } catch (final RollbackException e) {
+            throw new ClientErrorException(CONFLICT);
+        } finally {
+            entityManager.getTransaction().begin();
+        }
+        // MERKE: wenn sich Spiegelrelationen verändern, muss
+        // die betroffene Entität aus dem second level cache entfernt werden
+        return order.getIdentity();
     }
 
     @GET
